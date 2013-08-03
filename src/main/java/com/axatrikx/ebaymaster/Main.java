fd1caae17471e4173632bf98410e8b0dd246195b
Amal Bose <axatrikx@gmail.com>
2013-08-03 20:05:18 +0530

    Added dummy Main

---
+++ src/main/java/com/axatrikx/ebaymaster/Main.java
@@ -0,0 +1,14 @@
+package com.axatrikx.ebaymaster;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+
+	static Logger logger = Logger.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		logger.debug("Debug trial");
+	}
+
+}
