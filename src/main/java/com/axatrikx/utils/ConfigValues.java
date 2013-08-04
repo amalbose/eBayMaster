931e9530b2828cf865ef34df90612c67638c13cb
Amal Bose <axatrikx@gmail.com>
2013-08-04 19:38:44 +0530

    Added values for Query folder and file names

--- src/main/java/com/axatrikx/utils/ConfigValues.java
+++ src/main/java/com/axatrikx/utils/ConfigValues.java
@@ -3,7 +3,10 @@ package com.axatrikx.utils;
 public enum ConfigValues {
 
 	SETTINGS_FILE("settings.properties"), 
-	CONFIG_FOLDER("CONFIG_FOLDER");
+	CONFIG_FOLDER("CONFIG_FOLDER"),
+	QUERY_FOLDER("db"),
+	QUERY_CREATE_FILE("dbqueries_create_tables.db"),
+	SEPARATOR("//");
 	
 	private String value;
 	
