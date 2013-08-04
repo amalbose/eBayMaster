dad48f7b2b9799b3f2562974f9b2814d0286d987
Amal Bose <axatrikx@gmail.com>
2013-08-04 21:47:14 +0530

    Minor modifications in ConfigValues

--- src/main/java/com/axatrikx/utils/ConfigValues.java
+++ src/main/java/com/axatrikx/utils/ConfigValues.java
@@ -2,10 +2,11 @@ package com.axatrikx.utils;
 
 public enum ConfigValues {
 
-	SETTINGS_FILE("settings.properties"), 
 	CONFIG_FOLDER("CONFIG_FOLDER"),
 	QUERY_FOLDER("db"),
+	SETTINGS_FILE("settings.properties"), 
 	QUERY_CREATE_FILE("dbqueries_create_tables.db"),
+	QUERY_SELECT_FILE("dbqueries_select.db"),
 	SEPARATOR("//");
 	
 	private String value;
