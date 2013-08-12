e4ee9d6c84096943de5fd062da92329363eb68fa
Amal Bose <axatrikx@gmail.com>
2013-08-12 22:06:42 +0530

    Modified to take size from property file

--- src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
+++ src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
@@ -6,12 +6,23 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.table.TableColumn;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import com.axatrikx.controllers.TransactionsTableModel;
+import com.axatrikx.utils.CommonSettings;
 
 public class TransactionsPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7194810207131313783L;
+
 	private JTable table;
+
+	private static final String QUERY_TRANS_DETAIL_TKN = "QUERY_TRANS_DETAIL";
+	private static final String DEFAULT_WINDOW_WIDTH_TKN = "DEFAULT_WINDOW_WIDTH";
+	private static final String DEFAULT_WINDOW_HEIGHT_TKN = "DEFAULT_WINDOW_HEIGHT";
 
 	/**
 	 * Create the panel.
@@ -19,33 +30,32 @@ public class TransactionsPanel extends JPanel {
 	 * @throws Exception
 	 */
 	public TransactionsPanel() throws Exception {
-
-		/*
-		 * String query = "Select * from EBAYMASTERDB.TRANSACTIONS"; try { table = new JTable(new
-		 * TransactionsTableModel(query)); } catch (Exception e) { // TODO Auto-generated catch block
-		 * e.printStackTrace(); }
-		 */
-
 		table = new JTable();
-		table.setModel(new TransactionsTableModel(
-				"SELECT T.TRANSACTIONID, T.ITEMID, T.BUYERNAME, T.LOCATION, T.COST, T.PRICE, T.PROFIT, T.DATE, I.ITEMNAME, I.CATEGORY, I.RATE FROM EBAYMASTERDB.TRANSACTIONS AS T, EBAYMASTERDB.ITEMS AS I WHERE I.ITEMID = T.ITEMID"));
-		table.setPreferredScrollableViewportSize(new Dimension(1000, 400));
-        table.setFillsViewportHeight(true);
+		UIManager.setLookAndFeel(
+	            UIManager.getSystemLookAndFeelClassName());
+		// Getting query for model.
+		table.setModel(new TransactionsTableModel(CommonSettings.getDBInsertQuery(QUERY_TRANS_DETAIL_TKN)));
+		
+		// Settings the window size.
+		table.setPreferredScrollableViewportSize(new Dimension(Integer.parseInt(CommonSettings
+				.getSettingValue(DEFAULT_WINDOW_WIDTH_TKN)), Integer.parseInt(CommonSettings
+				.getSettingValue(DEFAULT_WINDOW_HEIGHT_TKN))));
+		table.setFillsViewportHeight(true);
 
-        //Create the scroll pane and add the table to it.
-        JScrollPane scrollPane = new JScrollPane(table);
-        //Add the scroll pane to this panel.
-        add(scrollPane);
-        
-		/*for (int i = 0; i < colCount; i++) {
-			TableColumn column = table.getColumnModel().getColumn(i);
-			column.setPreferredWidth(250);
-		}*/
+		// Create the scroll pane and add the table to it.
+		JScrollPane scrollPane = new JScrollPane(table);
+		// Add the scroll pane to this panel.
+		add(scrollPane);
+		/*
+		 * for (int i = 0; i < colCount; i++) { TableColumn column = table.getColumnModel().getColumn(i);
+		 * column.setPreferredWidth(250); }
+		 */
 	}
 
 	public static void main(String[] args) {
 		// Schedule a job for the event-dispatching thread:
 		// creating and showing this application's GUI.
+		
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				createAndShowGUI();
@@ -55,6 +65,7 @@ public class TransactionsPanel extends JPanel {
 
 	private static void createAndShowGUI() {
 		// Create and set up the window.
+		
 		JFrame frame = new JFrame("TableDemo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
