b1738235086901504f6bb3b9b338212628df5bcc
Amal Bose <axatrikx@gmail.com>
2013-08-13 22:32:53 +0530

    Moved updates to displayUI function

--- src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
+++ src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
@@ -1,6 +1,7 @@
 package com.axatrikx.ui.panels;
 
 import java.awt.Dimension;
+import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -31,31 +32,30 @@ public class TransactionsPanel extends JPanel {
 	 */
 	public TransactionsPanel() throws Exception {
 		table = new JTable();
-		UIManager.setLookAndFeel(
-	            UIManager.getSystemLookAndFeelClassName());
+		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		displayUI();
+	}
+
+	private void displayUI() throws Exception {
 		// Getting query for model.
-		table.setModel(new TransactionsTableModel(CommonSettings.getDBInsertQuery(QUERY_TRANS_DETAIL_TKN)));
+		table.setModel(new TransactionsTableModel(CommonSettings.getDBSelectQuery(QUERY_TRANS_DETAIL_TKN)));
+		table.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
 		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		// Settings the window size.
 		table.setPreferredScrollableViewportSize(new Dimension(Integer.parseInt(CommonSettings
 				.getSettingValue(DEFAULT_WINDOW_WIDTH_TKN)), Integer.parseInt(CommonSettings
 				.getSettingValue(DEFAULT_WINDOW_HEIGHT_TKN))));
 		table.setFillsViewportHeight(true);
-
 		// Create the scroll pane and add the table to it.
 		JScrollPane scrollPane = new JScrollPane(table);
 		// Add the scroll pane to this panel.
 		add(scrollPane);
-		/*
-		 * for (int i = 0; i < colCount; i++) { TableColumn column = table.getColumnModel().getColumn(i);
-		 * column.setPreferredWidth(250); }
-		 */
 	}
 
 	public static void main(String[] args) {
 		// Schedule a job for the event-dispatching thread:
 		// creating and showing this application's GUI.
-		
+
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				createAndShowGUI();
@@ -65,7 +65,7 @@ public class TransactionsPanel extends JPanel {
 
 	private static void createAndShowGUI() {
 		// Create and set up the window.
-		
+
 		JFrame frame = new JFrame("TableDemo");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
