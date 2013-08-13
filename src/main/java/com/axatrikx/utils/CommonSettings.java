bb409a229aa038bd76f1d6522ce6f29a1d198ad1
Amal Bose <axatrikx@gmail.com>
2013-08-13 22:35:06 +0530

    Changed getDBInsertQuery function name to getDBSelectQuery

--- src/main/java/com/axatrikx/utils/CommonSettings.java
+++ src/main/java/com/axatrikx/utils/CommonSettings.java
@@ -49,7 +49,7 @@ public class CommonSettings {
 	 *            Query types include QUERY_TRANS_TABLE, QUERY_ITEMS_TABLE and QUERY_TRANS_DETAIL
 	 * @return The query string based on query type.
 	 */
-	public static String getDBInsertQuery(String queryType) {
+	public static String getDBSelectQuery(String queryType) {
 		Properties props = new Properties();
 		String fileName = ConfigValues.QUERY_FOLDER.toString() + ConfigValues.SEPARATOR.toString()
 				+ ConfigValues.QUERY_SELECT_FILE.toString();
