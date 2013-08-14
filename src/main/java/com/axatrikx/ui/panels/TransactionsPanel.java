048f0360b15166cedb7d38fce432fea2d9b1a3ae
Amal Bose <axatrikx@gmail.com>
2013-08-14 21:07:28 +0530

    removed default look and feel for panel

--- src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
+++ src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
@@ -32,7 +32,6 @@ public class TransactionsPanel extends JPanel {
 	 */
 	public TransactionsPanel() throws Exception {
 		table = new JTable();
-		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		displayUI();
 	}
 
@@ -51,38 +50,4 @@ public class TransactionsPanel extends JPanel {
 		// Add the scroll pane to this panel.
 		add(scrollPane);
 	}
-
-	public static void main(String[] args) {
-		// Schedule a job for the event-dispatching thread:
-		// creating and showing this application's GUI.
-
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				createAndShowGUI();
-			}
-		});
-	}
-
-	private static void createAndShowGUI() {
-		// Create and set up the window.
-
-		JFrame frame = new JFrame("TableDemo");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		// Create and set up the content pane.
-		TransactionsPanel newContentPane = null;
-		try {
-			newContentPane = new TransactionsPanel();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		newContentPane.setOpaque(true); // content panes must be opaque
-		frame.setContentPane(newContentPane);
-
-		// Display the window.
-		frame.pack();
-		frame.setVisible(true);
-	}
-
 }
