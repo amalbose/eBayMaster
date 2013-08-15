c052dac406a7e17e09320c08e39fe3e75bcf7c4a
Amal Bose <axatrikx@gmail.com>
2013-08-15 22:47:41 +0530

    Added ErrorDialog class

---
+++ src/main/java/com/axatrikx/ui/panels/ErrorDialog.java
@@ -0,0 +1,65 @@
+package com.axatrikx.ui.panels;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import net.miginfocom.swing.MigLayout;
+import java.awt.Component;
+import javax.swing.Box;
+
+public class ErrorDialog extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			ErrorDialog dialog = new ErrorDialog("Title","Some thing happened");
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public ErrorDialog(String title, String errorMsg) {
+		setBounds(100, 100, 450, 175);
+		getContentPane().setLayout(new BorderLayout());
+		setTitle(title);
+		setType(Type.POPUP);
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new MigLayout("", "[27.00px][]", "[29.00px][]"));
+		{
+			JLabel lblNewLabel = new JLabel(errorMsg);
+			contentPanel.add(lblNewLabel, "cell 1 1,alignx left,aligny top");
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+}
