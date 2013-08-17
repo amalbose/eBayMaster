8aed0778eb83cb9c485e2c8b2d1a54a01f410cc2
Amal Bose <axatrikx@gmail.com>
2013-08-17 18:47:26 +0530

    Added PreparedDataExecutor to handle preparedStatements

---
+++ src/main/java/com/axatrikx/utils/PreparedDataExecutor.java
@@ -0,0 +1,172 @@
+package com.axatrikx.utils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+import com.axatrikx.beans.QueryResultTable;
+import com.axatrikx.db.DatabaseController;
+import com.axatrikx.errors.DataBaseException;
+import com.axatrikx.errors.DatabaseTableCreationException;
+
+public class PreparedDataExecutor {
+
+	private static Logger log = Logger.getLogger(PreparedDataExecutor.class);
+
+	@SuppressWarnings("rawtypes")
+	private ArrayList<HashMap<Class, Object>> dataList;
+	private String preparedStatement;
+	private Connection dbConnection;
+
+	@SuppressWarnings("rawtypes")
+	public PreparedDataExecutor(Connection dbConnection, ArrayList<HashMap<Class, Object>> dataList, String preparedStatement) {
+		this.dataList = dataList;
+		this.preparedStatement = preparedStatement;
+		this.dbConnection = dbConnection;
+	}
+
+	@SuppressWarnings("rawtypes")
+	public PreparedStatement getPreparedStatement() {
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = dbConnection.prepareStatement(this.preparedStatement);
+			int index = 0;
+			for (HashMap<Class, Object> data : dataList) {
+				Set<Class> keySet = data.keySet();
+				if (!data.isEmpty()) {
+					for (Class<?> dataType : keySet) {
+						if (dataType.isAssignableFrom(Integer.class)) {
+							preparedStatement.setInt(index + 1, (Integer) data.get(dataType));
+							log.info("Added Integer " + data.get(dataType));
+						} else if (dataType.isAssignableFrom(Float.class)) {
+							preparedStatement.setFloat(index + 1, (Float) data.get(dataType));
+							log.info("Added Float " + data.get(dataType));
+						} else if (dataType.isAssignableFrom(String.class)) {
+							preparedStatement.setString(index + 1, (String) data.get(dataType));
+							log.info("Added String " + data.get(dataType));
+						} else if (dataType.isAssignableFrom(Timestamp.class)) {
+							preparedStatement.setTimestamp(index + 1, (Timestamp) data.get(dataType));
+							log.info("Added Time " + data.get(dataType));
+						} else {
+							// TODO throw exception.
+						}
+					}
+				}
+				index++;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return preparedStatement;
+	}
+
+	/**
+	 * Execute query and returns the result table in 2D arraylist format.
+	 * 
+	 * @param queryString
+	 *            The query to be executed
+	 * @return The result table in 2D arraylist format.
+	 * @throws DataBaseException
+	 */
+	public QueryResultTable executeQueryForResult()
+			throws DataBaseException {
+		// result object
+		QueryResultTable queryResult = new QueryResultTable();
+
+		// 2D arraylist for result table
+		ArrayList<ArrayList<String>> resultTable = new ArrayList<ArrayList<String>>();
+		// hashmap for header details
+		LinkedHashMap<String, String> headers = new LinkedHashMap<String, String>();
+
+		try {
+			ResultSet rs = getPreparedStatement().executeQuery();
+			int colSize = rs.getMetaData().getColumnCount();
+
+			// setting header details.
+			for (int i = 1; i <= colSize; i++) {
+				headers.put(rs.getMetaData().getColumnLabel(i), rs.getMetaData().getColumnTypeName(i));
+			}
+
+			if (headers.size() < 1) {
+				log.warn("No columns(ColSize: " + colSize + ") were obtained from query");
+			}
+			queryResult.setHeaderDetails(headers);
+			ArrayList<String> rowData;
+			while (rs.next()) {
+				rowData = new ArrayList<String>();
+				for (int i = 1; i <= colSize; i++) {
+					rowData.add(String.valueOf(rs.getObject(i)));
+				}
+				resultTable.add(rowData);
+			}
+
+			if (resultTable.size() < 1) {
+				log.warn("No results(Result Count: " + resultTable.size() + " were obtained from query");
+			}
+
+		} catch (SQLException e) {
+			log.error("SQLException while executing query", e);
+			throw new DataBaseException(e.getMessage(), e);
+		} finally {
+			try {
+				dbConnection.close();
+			} catch (SQLException e) {
+				log.error("SQLException while closing connection", e);
+				throw new DataBaseException(e.getMessage(), e);
+			}
+		}
+		queryResult.setResultTable(resultTable);
+		System.out.println(resultTable);
+		return queryResult;
+	}
+
+	public static void main(String[] args) throws ClassNotFoundException, DataBaseException,
+			DatabaseTableCreationException, SQLException {
+		String query = "INSERT INTO EBAYMASTERDB.TRANSACTIONS(ITEMID, BUYERNAME, LOCATION, COST, PRICE, PROFIT, DATE) VALUES (?,?,?,?,?,?,?)";
+
+		ArrayList<HashMap<Class, Object>> dataList = new ArrayList<HashMap<Class, Object>>();
+
+		HashMap<Class, Object> itemIdVal = new HashMap<Class, Object>();
+		itemIdVal.put(Integer.class, 2);
+		dataList.add(itemIdVal);
+
+		HashMap<Class, Object> buyerNameVal = new HashMap<Class, Object>();
+		buyerNameVal.put(String.class, "New buyer");
+		dataList.add(buyerNameVal);
+
+		HashMap<Class, Object> locationVal = new HashMap<Class, Object>();
+		locationVal.put(String.class, "new Location");
+		dataList.add(locationVal);
+
+		HashMap<Class, Object> costVal = new HashMap<Class, Object>();
+		costVal.put(Float.class, 1.0f);
+		dataList.add(costVal);
+
+		HashMap<Class, Object> priceVal = new HashMap<Class, Object>();
+		priceVal.put(Float.class, 2.0f);
+		dataList.add(priceVal);
+
+		HashMap<Class, Object> profitVal = new HashMap<Class, Object>();
+		profitVal.put(Float.class, 1.2f);
+		dataList.add(profitVal);
+
+		HashMap<Class, Object> dateVal = new HashMap<Class, Object>();
+		dateVal.put(Timestamp.class, new Timestamp(new Date().getTime()));
+		dataList.add(dateVal);
+
+		new PreparedDataExecutor(new DatabaseController().getConnection(), dataList, query).getPreparedStatement()
+				.executeUpdate();
+	}
+
+}
