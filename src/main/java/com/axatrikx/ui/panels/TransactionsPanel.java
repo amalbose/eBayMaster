e8454547da5e2ea56f71d873d41b830b118e327e
Amal Bose <axatrikx@gmail.com>
2013-08-17 11:57:04 +0530

    1. Added jtable context menu 2. completed edit context menu item functionality 3. Other minor modifications

--- src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
+++ src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
@@ -1,13 +1,21 @@
 package com.axatrikx.ui.panels;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.EventObject;
 
+import javax.swing.DefaultCellEditor;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
@@ -15,7 +23,6 @@ import javax.swing.table.TableColumnModel;
 
 import com.axatrikx.controllers.TransactionController;
 import com.axatrikx.controllers.TransactionsTableModel;
-import com.axatrikx.utils.CommonSettings;
 
 public class TransactionsPanel extends JPanel {
 	/**
@@ -24,6 +31,8 @@ public class TransactionsPanel extends JPanel {
 	private static final long serialVersionUID = 7194810207131313783L;
 
 	private JTable table;
+	private int selectedRow = -1;
+	private int selectedColumn = -1;
 
 	private static final String QUERY_TRANS_DETAIL_TKN = "QUERY_TRANS_DETAIL";
 
@@ -47,6 +56,8 @@ public class TransactionsPanel extends JPanel {
 				return component;
 			}
 		};
+		table.setDragEnabled(true);
+		table.setAutoCreateRowSorter(true);
 		displayUI();
 	}
 
@@ -55,8 +66,9 @@ public class TransactionsPanel extends JPanel {
 		table.setModel(new TransactionsTableModel(TransactionController.getDBSelectQuery(QUERY_TRANS_DETAIL_TKN)));
 		table.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
 		TableColumnModel tcm = table.getColumnModel();
-		hideColumn(tcm, TransactionsTableModel.getColumnNames()[0]);
-		hideColumn(tcm, TransactionsTableModel.getColumnNames()[1]);
+
+		decorateTable(tcm);
+
 		// Settings the window size.
 		table.setFillsViewportHeight(true);
 		// Create the scroll pane and add the table to it.
@@ -64,6 +76,62 @@ public class TransactionsPanel extends JPanel {
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		// Add the scroll pane to this panel.
 		add(scrollPane, BorderLayout.CENTER);
+	}
+
+	private void decorateTable(TableColumnModel tcm) {
+
+		hideColumn(tcm, TransactionsTableModel.getColumnNames()[0]);
+		hideColumn(tcm, TransactionsTableModel.getColumnNames()[1]);
+
+		table.setDefaultEditor(Object.class, new DefaultCellEditor(new JTextField()) {
+
+			private static final long serialVersionUID = 3524303979037876874L;
+
+			public boolean isCellEditable(EventObject e) {
+				if (e instanceof MouseEvent) {
+					if (((MouseEvent) e).getClickCount() == 2 && !((MouseEvent) e).isConsumed()) {
+						((MouseEvent) e).consume();
+						return true;
+					}
+				} else if (selectedRow != -1 && selectedColumn != -1) {
+					return true;
+				}
+				return false;
+			}
+		});
+
+		// context menu
+		final JPopupMenu popupMenu = new JPopupMenu();
+		JMenuItem editItem = new JMenuItem("Edit");
+		JMenuItem deleteItem = new JMenuItem("Delete");
+
+		editItem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				table.editCellAt(selectedRow, selectedColumn);
+			}
+		});
+		popupMenu.add(editItem);
+		popupMenu.add(deleteItem);
+		table.setComponentPopupMenu(popupMenu);
+
+		table.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent me) {
+				showPopup(me);
+			}
+
+			public void mouseReleased(MouseEvent me) {
+				// showPopup(me);
+			}
+
+			private void showPopup(MouseEvent me) {
+				// is this event a popup trigger?
+				if (me.getButton() == MouseEvent.BUTTON3) {
+					Point p = me.getPoint();
+					selectedRow = table.rowAtPoint(p);
+					selectedColumn = table.columnAtPoint(p);
+				}
+			}
+		});
 	}
 
 	private void hideColumn(TableColumnModel tcm, String columnName) {
