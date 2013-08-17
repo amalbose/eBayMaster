da117b6ed310e949a14b0f11b4900fdf78fda5ba
Amal Bose <axatrikx@gmail.com>
2013-08-17 06:41:00 +0530

    Modified to obtain dimension from property file

--- src/main/java/com/axatrikx/ui/main/EBayMaster.java
+++ src/main/java/com/axatrikx/ui/main/EBayMaster.java
@@ -2,13 +2,17 @@ package com.axatrikx.ui.main;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import com.axatrikx.utils.CommonSettings;
+
 public class EBayMaster {
+
+	private static final String DEFAULT_WINDOW_WIDTH_TKN = "DEFAULT_WINDOW_WIDTH";
+	private static final String DEFAULT_WINDOW_HEIGHT_TKN = "DEFAULT_WINDOW_HEIGHT";
 
 	private JFrame mainFrame;
 
@@ -24,8 +28,7 @@ public class EBayMaster {
 	 */
 	private void initialize() {
 		try {
-			UIManager.setLookAndFeel(
-			        UIManager.getSystemLookAndFeelClassName());
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -40,15 +43,15 @@ public class EBayMaster {
 			e.printStackTrace();
 		}
 		mainFrame = new MainFrame();
-		mainFrame.getContentPane().setSize(new Dimension(1000, 500));
-		mainFrame.getContentPane().setPreferredSize(new Dimension(1000, 500));
-		mainFrame.setSize(new Dimension(1000, 500));
-		mainFrame.setPreferredSize(new Dimension(1000, 500));
-		
+		Dimension windowDimension = new Dimension(Integer.parseInt(CommonSettings
+				.getSettingValue(DEFAULT_WINDOW_WIDTH_TKN)), Integer.parseInt(CommonSettings
+				.getSettingValue(DEFAULT_WINDOW_HEIGHT_TKN)));
+		mainFrame.setSize(windowDimension);
+		mainFrame.setPreferredSize(windowDimension);
+
 		mainFrame.setName("mainFrame");
 		mainFrame.setTitle("eBay Master");
 		mainFrame.setBackground(Color.WHITE);
-	//	mainFrame.setBounds(100, 100, 450, 300);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainFrame.setVisible(true);
 	}
