b19fc340f602ad74b0be5baa59851a5e4dac39c3
Amal Bose <axatrikx@gmail.com>
2013-08-17 14:00:26 +0530

    Added getDBInsertQuery method

--- src/main/java/com/axatrikx/controllers/TransactionController.java
+++ src/main/java/com/axatrikx/controllers/TransactionController.java
@@ -20,12 +20,14 @@ public class TransactionController {
 	private static final String QUERY_ITEMS_TABLE_TKN = "QUERY_ITEMS_TABLE";
 	private static final String QUERY_TRANS_DETAIL_TKN = "QUERY_TRANS_DETAIL";
 
+	private static final String INSERT_TRANS_TABLE_TKN = "INSERT_TRANS_TABLE";
+
 	public void getTransactionModel(String queryString) {
 
 	}
 
 	/**
-	 * Returns the query based on the type of query. Types include 'QUERY_TRANS_TABLE', 'QUERY_ITEMS_TABLE' and
+	 * Returns the select query based on the type of query. Types include 'QUERY_TRANS_TABLE', 'QUERY_ITEMS_TABLE' and
 	 * 'QUERY_TRANS_DETAIL'.
 	 * 
 	 * @param queryType
@@ -47,14 +49,34 @@ public class TransactionController {
 	}
 
 	/**
+	 * Returns the insert query based on the type of query. Types include 'INSERT_TRANS_TABLE', 'INSERT_ITEMS_TABLE'.
+	 * 
+	 * @param queryType
+	 *            Which query to return. Types include 'INSERT_TRANS_TABLE', 'INSERT_ITEMS_TABLE'.
+	 * @return The query string.
+	 */
+	public static String getDBInsertQuery(String tableType) {
+		String result = null;
+		Properties queryValues = CommonSettings.getPropertiesFromFile(ConfigValues.QUERY_FOLDER.toString()
+				+ ConfigValues.SEPARATOR.toString() + ConfigValues.QUERY_INSERT_FILE.toString());
+		if (INSERT_TRANS_TABLE_TKN.equals(tableType)) {
+			result = queryValues.getProperty(tableType);
+		} else {
+			log.error("Invalid tableType: " + tableType);
+		}
+		return result;
+	}
+
+	/**
 	 * Gets the transaction item categories as a list.
 	 * 
 	 * @return Categories as a list.
-	 * @throws DataBaseException 
-	 * @throws ClassNotFoundException 
-	 * @throws DatabaseTableCreationException 
+	 * @throws DataBaseException
+	 * @throws ClassNotFoundException
+	 * @throws DatabaseTableCreationException
 	 */
-	public static List<String> getCategories() throws ClassNotFoundException, DataBaseException, DatabaseTableCreationException {
+	public static List<String> getCategories() throws ClassNotFoundException, DataBaseException,
+			DatabaseTableCreationException {
 		ArrayList<String> res = new ArrayList<String>();
 		ArrayList<ArrayList<String>> resultTable = new DatabaseController().executeQueryForResult(
 				"SELECT DISTINCT CATEGORY FROM " + DatabaseController.getDatabaseName() + ".ITEMS").getResultTable();
