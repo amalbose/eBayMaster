53e07401773846168a32a2de8c75da981ed25324
Amal Bose <axatrikx@gmail.com>
2013-08-17 06:43:36 +0530

    Removed setPreferredViewPortSize to make jtable fill the parent

--- src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
+++ src/main/java/com/axatrikx/ui/panels/TransactionsPanel.java
@@ -1,11 +1,14 @@
 package com.axatrikx.ui.panels;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableColumnModel;
@@ -23,8 +26,6 @@ public class TransactionsPanel extends JPanel {
 	private JTable table;
 
 	private static final String QUERY_TRANS_DETAIL_TKN = "QUERY_TRANS_DETAIL";
-	private static final String DEFAULT_WINDOW_WIDTH_TKN = "DEFAULT_WINDOW_WIDTH";
-	private static final String DEFAULT_WINDOW_HEIGHT_TKN = "DEFAULT_WINDOW_HEIGHT";
 
 	/**
 	 * Create the panel.
@@ -32,18 +33,19 @@ public class TransactionsPanel extends JPanel {
 	 * @throws Exception
 	 */
 	public TransactionsPanel() throws Exception {
-		table = new JTable(){
-			 @Override
-	         public Component prepareRenderer(TableCellRenderer renderer, int row,
-	                 int column) {
-	            Component component = super.prepareRenderer(renderer, row, column);
-	            int rendererWidth = component.getPreferredSize().width;
-	            TableColumn tableColumn = getColumnModel().getColumn(column);
-	            tableColumn.setPreferredWidth(Math.max(rendererWidth +
-	                    getIntercellSpacing().width,
-	                    tableColumn.getPreferredWidth()));
-	            return  component;
-	         }
+		setLayout(new BorderLayout());
+		table = new JTable() {
+
+			private static final long serialVersionUID = 8390383435099848025L;
+
+			public Component prepareRenderer(TableCellRenderer renderer, int row, int column) {
+				Component component = super.prepareRenderer(renderer, row, column);
+				int rendererWidth = component.getPreferredSize().width;
+				TableColumn tableColumn = getColumnModel().getColumn(column);
+				tableColumn.setPreferredWidth(Math.max(rendererWidth + getIntercellSpacing().width,
+						tableColumn.getPreferredWidth()));
+				return component;
+			}
 		};
 		displayUI();
 	}
@@ -52,20 +54,16 @@ public class TransactionsPanel extends JPanel {
 		// Getting query for model.
 		table.setModel(new TransactionsTableModel(TransactionController.getDBSelectQuery(QUERY_TRANS_DETAIL_TKN)));
 		table.putClientProperty("terminateEditOnFocusLost", Boolean.TRUE);
-		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		TableColumnModel tcm = table.getColumnModel();
-		tcm.getColumn(0).setWidth(160);
 		hideColumn(tcm, TransactionsTableModel.getColumnNames()[0]);
 		hideColumn(tcm, TransactionsTableModel.getColumnNames()[1]);
 		// Settings the window size.
-		table.setPreferredScrollableViewportSize(new Dimension(Integer.parseInt(CommonSettings
-				.getSettingValue(DEFAULT_WINDOW_WIDTH_TKN)), Integer.parseInt(CommonSettings
-				.getSettingValue(DEFAULT_WINDOW_HEIGHT_TKN))));
 		table.setFillsViewportHeight(true);
 		// Create the scroll pane and add the table to it.
 		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		// Add the scroll pane to this panel.
-		add(scrollPane);
+		add(scrollPane, BorderLayout.CENTER);
 	}
 
 	private void hideColumn(TableColumnModel tcm, String columnName) {
